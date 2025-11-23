921cdfdc947d10899a53b71902593bd74055cc8f
shivamtiwari45 <shivamtiwarybxr05@gmail.com>
2025-11-23 14:13:56 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -36,5 +36,31 @@ let negValue = -value
 let str1 = "Shivam"
 let str2 = " Tiwari"
 let str3 = str1 + str2
-console.log(str3);
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+ // https://tc39.es/ecma262/2023/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
 
