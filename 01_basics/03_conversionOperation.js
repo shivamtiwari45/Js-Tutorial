e0b60f78a41a7d5e0851005769366e6bcbbefb84
shivamtiwari45 <shivamtiwarybxr05@gmail.com>
2025-11-23 13:36:08 +0000

    {commit_message}

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = "Shivam"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInnumber);
+
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1; false -> 0
+
+let isLoggedIn = "Shivam"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 -> true; 0 -> false
+// "" -> false
+// "Shivam" -> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************** Operations ******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Shivam"
+let str2 = " Tiwari"
+let str3 = str1 + str2
+console.log(str3);
+
